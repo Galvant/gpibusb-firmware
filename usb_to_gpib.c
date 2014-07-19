Galvant/gpibusb-firmware

commit b1b79aeb979cd058337d7ddfaa24b139d6848b96

Manually merged ++auto fix from commit 430b3ce1

usb_to_gpib.c
@@ -830,8 +830,8 @@ void main(void) {
 				            }
 				            else if (*(buf_pnt+6) == 32) {
 				                autoread = atoi((char*)(buf_pnt+7));
-				                if ((autoread != 0) && (autoread != 1)) {
-				                    autoread = 1; // If non-bool sent, set to enable
+				                if (autoread > 2) {
+				                    autoread = 2;    
 				                }
 				            }
 				        }
@@ -1073,6 +1073,7 @@ void main(void) {
 			        else if(*(buf_pnt+2) == 'u') {
 			            // +autoread:
 			            autoread = atoi((char*)(buf_pnt+10));
+			            if (autoread == 1) { autoread = 2; }
 			        }
 			    }
 			    else if(*(buf_pnt+1) == 'd') {
@@ -1177,14 +1178,17 @@ void main(void) {
 				}
 				
 				// If cmd contains a question mark -> is a query
-				if(autoread && mode) {
-				    if ((strchr((char*)buf_pnt, '?') != NULL) && !(writeError)) { 
+				if((mode) && !(writeError)) {
+				    if(autoread == 1) {
+				        gpib_read(eoiUse);
+			        }
+				    else if ((autoread == 2) && (strchr((char*)buf_pnt, '?') != NULL)) { 
 					    gpib_read(eoiUse);					    
 				    }
-				    else if(writeError){
-					    writeError = 0;
-				    }
 				}
+				else if(writeError){
+				    writeError = 0;
+			    }
 			} // end of sending internal command
 
 		} // End of receiving PC input

commit ee768e24077625c8128651dab5b5af52855cb297

Added comments re:speed regression from commit b1b79aeb

usb_to_gpib.c
@@ -1255,13 +1255,17 @@ void main(void) {
                     }
                 }
                 
-                // If cmd contains a question mark -> is a query
+                // Check to see if adapter should automatically tell
+                // instrument to talk
                 if((mode) && !(writeError)) {
-                    if(autoread == 1) {
-                        gpib_read(eoiUse);
-                    }
-                    else if ((autoread == 2) && (strchr((char*)buf_pnt, '?') != NULL)) { 
+			        if ((strchr((char*)buf_pnt, '?') != NULL) && (autoread==2)){ 
                         gpib_read(eoiUse);                        
+                    }
+                    // This next line is slower than the above if statement
+                    // and I have no idea why. Just removing the strchr section
+                    // slows it down.
+                    else if (autoread==1) {
+                        gpib_read(eoiUse);
                     }
                 }
                 else if(writeError){

usb_to_gpib.hex
@@ -378,7 +378,7 @@
 :101770004CC1E9FF4DC1EAFF4E51EF5C12E04DC193
 :1017800003F04CC1E9FF4DC1EAFFEF5204E1000E46
 :10179000016E026E0AD04C2BD8B44D2B0001E5D758
-:1017A00001014CC101F04DC102F0000102EF17F040
+:1017A00001014CC101F04DC102F00001F6EF16F04D
 :1017B000F86AD09E0D8EEA6AE96AB886090EAF6EA5
 :1017C000000EB06EA60EAC6E900EAB6EC150C00B8C
 :1017D0000F09C16E070EB46E186A196A0101276BF2
@@ -733,11 +733,11 @@
 :102DA00049F1010116D0495311E14BC14DF14AC11E
 :102DB0004CF14E6B010E4F6F0001A6EC0BF001526F
 :102DC000D8B402D0010102D0000E01D0010E0101E1
-:102DD000496F385327E0495325E1342D07D031C1DD
-:102DE0004CF10001BDEC08F01BD001013451020888
-:102DF00018E14BC14DF14AC14CF13F0E4E6F00013D
-:102E0000B5EF0BF002C04DF101C04CF101014C5384
-:102E100002E14D5306E031C14CF10001BDEC08F078
+:102DD000496F385327E0495325E14BC14DF14AC1B2
+:102DE0004CF13F0E4E6F0001B5EF0BF002C04DF1FC
+:102DF00001C04CF101014C5302E14D530AE0345142
+:102E0000020807E131C14CF10001BDEC08F008D027
+:102E10000101342D06D031C14CF10001BDEC08F0A8
 :102E2000010103D0495301E0496B0001010138530E
 :102E3000D8A490D0928280B269D0928280B265D0BC
 :102E4000929A899ABA6B1E0EB96F000101EC08F0D4